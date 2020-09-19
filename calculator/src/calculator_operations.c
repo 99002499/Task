f5fde2172530ee4ff5f3581c9a91c11aa8df5345
99002499 <71490915+99002499@users.noreply.github.com>
2020-09-19 09:36:07 +0530

    Update calculator_operations.c

--- calculator/src/calculator_operations.c
+++ calculator/src/calculator_operations.c
@@ -1,246 +1,251 @@
 #include <calculator_operations.h>
 
 int add(int operand1, int operand2)
-{   
-    int result;
-    result= operand1 + operand2;
-    return result;
+{
+    return operand1 + operand2;
 }
 
 int subtract(int operand1, int operand2)
 {
-    int result;
-    result= operand1 - operand2;
-    return result;
+    return operand1 - operand2;
 }
 
 int multiply(int operand1, int operand2)
 {
-    int result;
-    result= operand1 * operand2;
-    return result;
+    return operand1 * operand2;
 }
 
 int divide(int operand1, int operand2)
 {
-    int result;
     if(0 == operand2)
         return 0;
     else
-        result= operand1 / operand2;
-    return result;
+        return operand1 / operand2;
 }
-int modulus(int operand1, int operand2)
-{
-    int result;
-    return operand1 % operand2;
-    return result;
-}
-int primenumber(int operand1)
-    {
-        int i,count=0;
-        if(operand1<0)
-        {
-            return -1;
-        }
-        for(i=1;i<=operand1;i++)
-        {
-            if(operand1%i==0)
-            {
-                count++;
-            }
-        }
-        if(count==2)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
+
 int greater(int operand1, int operand2)
 {
-    if(operand1 > operand2)
+    if(operand1>operand2)
         return operand1;
     else
         return operand2;
 }
+
 int smaller(int operand1, int operand2)
 {
-    if(operand1 < operand2)
+    if(operand1<operand2)
         return operand1;
     else
         return operand2;
 }
+
+int primenum(int operand1)
+{
+    int i,m=0,flag=0;    
+      
+    m=operand1/2;    
+    for(i=2;i<=m;i++)    
+    {    
+    if(operand1%i==0)    
+    {    
+    return 0;   
+    flag=1;    
+    break;    
+    }    
+    }    
+    if(flag==0)    
+    return 1;  
+}
+    
+int primenum1(int operand2)
+{
+    int i,m=0,flag=0;    
+      
+    m=operand2/2;    
+    for(i=2;i<=m;i++)    
+    {    
+    if(operand2%i==0)    
+    {    
+    return 0;   
+    flag=1;    
+    break;    
+    }    
+    }    
+    if(flag==0)    
+    return 1;  
+}
+
+int evenodd(int operand1)
+{
+    if(operand1%2==0)
+        return 1;
+    else
+        return 0; 
+}
+
+int evenodd1(int operand2)
+{
+    if(operand2%2==0)
+        return 1;
+    else
+        return 0; 
+}
+
+int posneg(int operand1)
+{
+    if(operand1>0)
+        return 1;
+    else if(operand1<0)
+        return 0;
+    else
+        return -1;
+}
+
+int posneg1(int operand2)
+{
+    if(operand2>0)
+        return 1;
+    else if(operand2<0)
+        return 0;
+    else
+        return -1;
+}
+
+int factorial(int operand1)
+{
+   if(operand1 < 0)
+    return -1;
+ if(operand1 == 0)
+    return 1;
+return operand1 * factorial(operand1-1);
+}
+
+int factorial1(int operand2)
+{
+   if(operand2 < 0)
+    return -1;
+ if(operand2 == 0)
+    return 1;
+return operand2 * factorial(operand2-1);
+}
+
+int squarearea(int operand1)
+{
+    return operand1*operand1;
+}
+
+int squareperi(int operand1)
+{
+    return 4*operand1;
+}
+
 int square(int operand1)
 {
-    int result;
-    result= operand1 * operand1;
-    return result;
+    return operand1*operand1;
 }
-int and(int operand1, int operand2)
+int square1(int operand2)
 {
-    int result;
-    if(operand1<0 && operand2<0)
-    return -1;
-    else if(operand1>1 && operand2>1)
-    return 1;
-    else if(operand1==1 && operand2==1)
-    return 1;
-    else
-    return 0;
+    return operand2*operand2;
 }
-int or(int operand1, int operand2)
+int cube(int operand1)
 {
-    int result;
-    if(operand1<0 && operand2<0)
-    return -1;
-    else if(operand1>1 && operand2>1)
-    return 1;
-    else if(operand1==0 && operand2==0)
-    return 0;
-    else
-    return 1;
+    return operand1*operand1*operand1;
 }
-int not(int operand1)
+int cube1(int operand2)
 {
-    if(operand1==0)
-        return 1;
+    return operand2*operand2*operand2;
+}
+
+int reverse(int operand1)
+{
+    int rev=0,remainder;
+    while(operand1!=0){
+        remainder = operand1%10;
+        rev = rev*10+remainder;
+        operand1 /= 10;
+    }
+    return rev;
+}
+int reverse1(int operand2)
+{
+    int rev=0,remainder;
+    while(operand2!=0){
+        remainder = operand2%10;
+        rev = rev*10+remainder;
+        operand2 /= 10;
+    }
+    return rev;
+}
+int palindrome(int operand1)
+{
+    int rev=0,remainder,original;
+    original = operand1;
+    while(operand1!=0){
+        remainder = operand1%10;
+        rev = rev*10+remainder;
+        operand1 /= 10;
+    }
+    if(original==rev)
+         return 1;
     else
         return 0;
 }
-int nand(int operand1, int operand2)
+int palindrome1(int operand2)
 {
-    int result;
-    if(operand1<0 && operand2<0)
-    return -1;
-    else if(operand1>1 && operand2>1)
-    return 0;
-    else if(operand1==1 && operand2==1)
-    return 0;
+    int rev=0,remainder,original;
+    original = operand2;
+    while(operand2!=0){
+        remainder = operand2%10;
+        rev = rev*10+remainder;
+        operand2 /= 10;
+    }
+    if(original==rev)
+         return 1;
     else
-    return 1;
+        return 0;
 }
-int nor(int operand1, int operand2)
+int gcd(int operand1, int operand2)
 {
-    if(operand1<0 && operand2<0)
-    return -1;
-    else if(operand1>1 && operand2>1)
-    return 0;
-    else if(operand1==0 && operand2==0)
-    return 1;
-    else
-    return 0;
+    int count,gcd;
+    for(count=1;count<=operand1 && count<=operand2;++count)
+    {
+        if(operand1%count==0 && operand2%count==0)
+            gcd = count;
+    }
+    return gcd;
 }
-int factorial(int operand1)
-    {   
-        int fact=1;
-        if (operand1==0)
-        {
-            return 1;
-        }
-        if (operand1 < 0)
-        {
-            return 0;
-        }
-        else
-        {
-            for (int i = 1; i <= operand1; ++i)
-            {
-                fact *= i;
-            }
-        return fact;
-        }
-    }
-int adder3bit(int operand1, int operand2, int operand3)
-
+int lcm(int operand1, int operand2)
 {
-    if(operand1 < 0 || operand2 < 0 || operand3 < 0)
-          {
-             return -1;
-          }
-     else if (operand1 > 1 || operand2 > 1 || operand3 > 1)
-          {
-              return -1;
-           }
-     else
+    int maximum;
+    maximum=(operand1>operand2)?operand1:operand2;
+    while(1)
+    {
+        if(maximum%operand1==0 && maximum%operand2==0)
         {
-           if(operand1==0 && operand2==0 && operand3==0)
-              {
-               return 0;
-              }
-           else if(operand1==0 && operand2==0 && operand3==1)
-              {
-               return 1;
-              }
-            else if(operand1==0 && operand2==1 && operand3==0)
-               {
-               return 1;
-               }
-        else if(operand1==0 && operand2==1 && operand3==1)
-              {
-               return 0;
-               }
-       else if(operand1==1 && operand2==0 && operand3==0)
-               {
-               return 1;
-               }
-       else if(operand1==1 && operand2==0 && operand3==1)
-              {
-               return 0;
-               }
-       else if(operand1==1 && operand2==1 && operand3==0)
-               {
-               return 0;
-               }
-        else 
-              return 1;
-     }
- }
-int subtractor3bit(int operand1, int operand2, int operand3)
+            return maximum;
+            break;
+        }
+        ++maximum;
+    }
+}
+int modulus(int operand1,int operand2)
 {
-    if(operand1 < 0 || operand2 < 0 || operand3 < 0)
-    {
-        return -1;
-    }
-    else if (operand1 > 1 || operand2 > 1 || operand3 > 1)
-    {
-        return -1;
-    }
-    else
-    {
-        if(operand1==0 && operand2==0 && operand3==0)
-        {
-            return 0;
-        }
-        else if(operand1==0 && operand2==0 && operand3==1)
-        {
-            return 1;
-        }
-        else if(operand1==0 && operand2==1 && operand3==0)
-        {
-            return 1;
-        }
-        else if(operand1==0 && operand2==1 && operand3==1)
-        {
-            return 0;
-        }
-        else if(operand1==1 && operand2==0 && operand3==0)
-        {
-            return 1;
-        }
-        else if(operand1==1 && operand2==0 && operand3==1)
-        {
-            return 0;
-        }
-        else if(operand1==1 && operand2==1 && operand3==0)
-        {
-            return 0;
-        }
-        else if(operand1==1 && operand2==1 && operand3==1)
-        {
-            return 1;
-        }
-    }
+    int remainder;
+    remainder= operand1%operand2;
+    return remainder;
+}
+int kilometertometer(int operand1)
+{
+    int meter=operand1*1000;
+    return meter;
+}
+int kilotocentimeter(int operand1)
+{
+    int centimeter = operand1*100000;
+    return centimeter;
+}
+int kilotomillimeter(int operand1)
+{
+    int millimeter = operand1*1000000;
+    return millimeter;
 }

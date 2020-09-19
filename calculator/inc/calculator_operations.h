41fd4d9967ceafd4029ecd743b4f83dba34aa6e4
99002499 <71490915+99002499@users.noreply.github.com>
2020-09-19 09:33:37 +0530

    Update calculator_operations.h

--- calculator/inc/calculator_operations.h
+++ calculator/inc/calculator_operations.h
@@ -1,4 +1,4 @@
-/**
+/** 
 * @file calculator_operations.h
 * Calculator application with 4 mathematical operations
 *
@@ -12,119 +12,69 @@
 
 /**
 *  adds the operand1 and operand2 and returns the result
-* @param[in] operand1
-* @param[in] operand2
+* @param[in] operand1 
+* @param[in] operand2 
 * @return Result of operand1 + operand2
 */
-int addition(int operand1, int operand2);
+int add(int operand1, int operand2);
 
 /**
 *  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1
-* @param[in] operand2
+* @param[in] operand1 
+* @param[in] operand2 
 * @return Result of operand1 - operand2
 */
-int subtraction(int operand1, int operand2);
+int subtract(int operand1, int operand2);
 
 /**
 *  multiply the operand1, operand2 and returns the result
-* @param[in] operand1
-* @param[in] operand2
+* @param[in] operand1 
+* @param[in] operand2 
 * @return Result of operand1 * operand2
 */
-int multiplication(int operand1, int operand2);
+int multiply(int operand1, int operand2);
 
 
 /**
-* divides the operand1 by operand2 and returns the result
-* @param[in] operand1
-* @param[in] operand2
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
 */
-int division(int operand1, int operand2);
-/**
-* finds sine function value and returns the result
-* @param[in] operand1
-* @return float value of the sine(operand1)
-*/
-float sine(float  operand1);
-/**
-* finds cosine function value and returns the result
-* @param[in] operand1
-* @return float value of the cosine(operand1)
-*/
+int divide(int operand1, int operand2);
 
-float cosine(float operand1);
 /**
-* finds tangent function value and returns the result
-* @param[in] operand1
-* @return float value of the tangent(operand1)
+*  check greater the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the greater operand1 or operand2
 */
-
-float tangent(float x);
-/**
-* finds sineh function value and returns the result
-* @param[in] operand1
-* @return float value of the sinh(operand1)
-*/
-
-float sineh(float x);
-/**
-* finds cosineh function value and returns the result
-* @param[in] operand1
-* @return float value of the cosineh(operand1)
-*/
-
-float cosineh(float x);
-\/**
-* finds tangenth function value and returns the result
-* @param[in] operand1
-* @return float value of the tangenth(operand1)
-*/
-
-float tangenth(float x);
-/**
-* finds logten function value and returns the result
-* @param[in] operand1
-* @return float value of the logten(operand1)
-*/
-
-float logten(float x);
-/**
-* finds squareroot function value and returns the result
-* @param[in] operand1
-* @return float value of the squareroot(operand1)
-*/
-
-float squareroot(float x);
-/**
-* finds exponent function value and returns the result
-* @param[in] operand1
-* @return float value of the exponent(operand1)
-*/
-
-float exponent(float operand1);
-/**
-* finds power function value and returns the result
-* @param[in] operand1
-* @return float value of the power(operand1)
-*/
-
-float power(float x,float y);
-/**
-* finds factorial function value and returns the result
-* @param[in] operand1
-* @return float value of the factorial(operand1)
-*/
-int factorial(int x);
-/**
-* finds absolute function value and returns the result
-* @param[in] operand1
-* @return float value of the absolute(operand1)
-*/
-
-float absolute(float x,float y);
-
+int greater(int operand1, int operand2);
+int smaller(int operand1, int operand2);
+int primenum(int operand1);
+int primenum1(int operand2);
+int evenodd(int operand1);
+int evenodd1(int operand2);
+int posneg(int operand1);
+int posneg1(int operand2);
+int factorial(int operand1);
+int factorial1(int operand2);
+int squarearea(int operand1);
+int squareperi(int operand1);
+int square(int operand1);
+int square1(int operand2);
+int cube(int operand1);
+int cube1(int operand2);
+int reverse(int operand1);
+int reverse1(int operand2);
+int palindrome(int operand1);
+int palindrome1(int operand2);
+int gcd(int operand1, int operand2);
+int lcm(int operand1, int operand2);
+int modulus(int operand1, int operand2);
+int kilometertometer(int operand1);
+int kilotocentimeter(int operand1);
+int kilotomillimeter(int operand1);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
